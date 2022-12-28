b7acb028b7985135f8c1d13a2cb875d956283e2c
Wouter van der Velde (student) <s1160197@student.windesheim.nl>
2022-12-28 23:51:28 +0100

    updated naming and started setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gti',
+    version='0.1',
+    author='Wouter van der Velde',
+    url='https://github.com/WoutervdVelde',
+    scripts=['gti.py'],
+    python_requires='>=3.1'   
+)
