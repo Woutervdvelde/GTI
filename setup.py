b84748a626e781cdd6b1678e16399021dff1dc81
Wouter van der Velde (student) <s1160197@student.windesheim.nl>
2022-12-29 00:13:55 +0100

    added additional information

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 from setuptools import setup
 
 setup(
     name='gti',
     version='0.1',
+    description='A simple script that shows a Golf GTI when you accidentally type gti instead of git.',
     author='Wouter van der Velde',
+    author_email='woutervandervelde2000@hotmail.com',
     url='https://github.com/WoutervdVelde',
+    license='MIT',
     scripts=['gti.py'],
     python_requires='>=3.1'   
 )
