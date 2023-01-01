10a59aefada537c3f98926b24f37fac855e84978
Wouter van der Velde (student) <s1160197@student.windesheim.nl>
2023-01-01 10:42:35 +0100

    tried some more solutions

--- gti.py
+++ gti.py
@@ -1,33 +1,40 @@
+#!/usr/bin/env python3
 from tkinter import *
 
-#Create the window
-root = Tk()
-root.config(bg="red")
-root.overrideredirect(True)
-root.wm_attributes("-topmost", True)
-root.wm_attributes("-transparentcolor", "red")
+def main():
+    #Create the window
+    # root = Tk()
+    # root.config(bg="red")
+    # root.overrideredirect(True)
+    # root.wm_attributes("-topmost", True)
+    # root.wm_attributes("-transparentcolor", "red")
 
-#Load the image
-photo = PhotoImage(file="gti.png")
-width, height = photo.width(), photo.height()
+    # #Load the image
+    # photo = PhotoImage(file="gti.png")
+    # width, height = photo.width(), photo.height()
 
-#Place image on canvas for transparency
-canvas = Canvas(root, bg="red", width=width, height=height)
-canvas.pack()
-canvas.create_image(0, 0, image=photo, anchor=NW)
+    # #Place image on canvas for transparency
+    # canvas = Canvas(root, bg="red", width=width, height=height)
+    # canvas.pack()
+    # canvas.create_image(0, 0, image=photo, anchor=NW)
 
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = 0 - width
-y = screen_height - height
+    # screen_width = root.winfo_screenwidth()
+    # screen_height = root.winfo_screenheight()
+    # x = 0 - width
+    # y = screen_height - height
 
-#Move window from left to right
-shouldRun = True
-while shouldRun:
-    root.geometry(f"{width}x{height}+{x}+{y}")
-    root.update()
-    x += 1
-    if x > screen_width:
-        shouldRun = False
+    # #Move window from left to right
+    # shouldRun = True
+    # while shouldRun:
+    #     root.geometry(f"{width}x{height}+{x}+{y}")
+    #     root.update()
+    #     x += 1
+    #     if x > screen_width:
+    #         shouldRun = False
 
-root.destroy()
+    # root.destroy()
+
+    print("meow")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
+#python setup.py check
+
+#python setup.py sdist
+
+#python setup.py bdist_wheel
+
 #twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
 from setuptools import setup
 
 setup(
     name='gti',
-    version='0.1',
+    version='0.1.8.3',
     description='A simple script that shows a Golf GTI when you accidentally type gti instead of git.',
     author='Wouter van der Velde',
     author_email='woutervandervelde2000@hotmail.com',
     url='https://github.com/WoutervdVelde',
     license='MIT',
-    scripts=['gti.py'],
-    python_requires='>=3.1'   
+    python_requires='>=3.1',
+    entry_points = {
+        'console_scripts': ['gti=gti:main'],
+    },
 )
